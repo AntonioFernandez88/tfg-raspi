e05601155ce5fc5f2943c0b3b6bb3b0e623f3130
Gutyny <antoniofc88@gmail.com>
2016-07-02 13:44:09 +0200

    Crear cookies de 1 año y eliminación

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,23 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET home page. */
+router.post('/', function(req, res, next) {
+  console.log(req.cookie);
+});
+//PARA CARGAR LA VISTA
 router.get('/vincular', function(req, res, next) {
   res.render('vincular');
+  console.log('holA');
 
-  req.query.nombre;
-  req.query.mac;
+});
 
+//PARA RECOGER LOS DATOS Y REDIRIGIR
+router.post('/vincular', function(req, res) {
+	var name = res.cookie('Nombre', req.body.nombre, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
+	var dirmac = res.cookie('Mac', req.body.mac, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
+	var numserie = res.cookie('Nserie', req.body.nserie, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
+    
+    res.redirect('/');
 });
 
 module.exports = router;
