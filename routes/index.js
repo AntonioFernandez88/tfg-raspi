836b7ed055f9603357b48d64fc6ee43bfba921be
Gutyny <antoniofc88@gmail.com>
2016-07-03 23:22:17 +0200

    cambio a la hora de enviar datos del qr en el formulario

--- routes/index.js
+++ routes/index.js
@@ -13,18 +13,32 @@ router.post('/', function(req, res, next) {
 });
 //PARA CARGAR LA VISTA
 router.get('/vincular', function(req, res, next) {
-  res.render('vincular');
+	
+	//Si viene vacio carga la vista normal, si hay datos los guarda el formulario viene por get
+	var nombre = req.query.nombre || '';
+	var mac = req.query.mac || '';
+	var nserie = req.query.mac || '';
+
+ 
+	if ((nombre != '') && (mac != '') && (nserie != '')){
+		var name = res.cookie('Nombre', req.query.nombre, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
+		var dirmac = res.cookie('Mac', req.query.mac, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
+		var numserie = res.cookie('Nserie', req.query.nserie, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
+	    
+	    res.redirect('/');
+	}else{res.render('vincular');}
+  
 
 });
 
-//PARA RECOGER LOS DATOS Y REDIRIGIR
+/*//PARA RECOGER LOS DATOS CON POST DESDE EL FORMULARIO
 router.post('/vincular', function(req, res) {
 	var name = res.cookie('Nombre', req.body.nombre, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
 	var dirmac = res.cookie('Mac', req.body.mac, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
 	var numserie = res.cookie('Nserie', req.body.nserie, {expires: new Date(Date.now() + (3600 * 1000 * 24 * 365))});
     
     res.redirect('/');
-});
+});*/
 
 //PARA CARGAR LA VISTA
 router.get('/camara', function(req, res, next) {
